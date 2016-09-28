3359c924d8537f4a97c1742d2f0df6a6df191c2b
Jagraphob Jantakananuruk <toe15g@gmail.com>
2016-09-28 16:13:47 +1300

    cleaner home

--- app/components/home/home.controller.js
+++ app/components/home/home.controller.js
@@ -3,7 +3,8 @@ angular.module('watApp')
 
         var vm = this;
 
-        vm.latestNewsText = "We just had an event on "
+        vm.latestNewsText = "We just had an event on 25th of November"
         vm.quickDhamma = "Everything that has a beginning has an end";
+        vm.latestNewsPic = "/app/content/images/latestNews.jpg"
 
     }]);
